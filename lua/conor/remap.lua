6c37d878eb135ba846e97ebc64222c2fbad6007f
MrWaffle243 <conorlyons36@gmail.com>
2026-07-15 20:56:06 +0100

    Add some remaps from prev setup

--- lua/conor/remap.lua
+++ lua/conor/remap.lua
@@ -2,6 +2,10 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Move selected block up or down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 -- Recenters cursor after page up/down
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
@@ -14,4 +18,8 @@ vim.keymap.set({ "n", "i", "v" }, "<Left>", "")
 
 -- Tmux sessioniser
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+-- Disable auto comments
+vim.cmd('autocmd BufEnter * set formatoptions-=cro')
+vim.cmd('autocmd BufEnter * setlocal formatoptions-=cro')
 
