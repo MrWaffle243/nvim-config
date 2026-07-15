76a3f43a9ffa936bc5620fb2a20fe835c821d760
MrWaffle243 <conorlyons36@gmail.com>
2026-07-15 18:11:26 +0100

    Fix treesitter

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -1,21 +1,11 @@
-vim.pack.add({ { 
-    src = 'https://github.com/nvim-treesitter/nvim-treesitter',
-    branch = "main",
-} })
+vim.pack.add({
+  {
+    src = "https://github.com/nvim-treesitter/nvim-treesitter",
+  },
+})
 
--- 2. Defer setup so Neovim has time to find the downloaded plugin files
-vim.schedule(function()
-    local ts = require("nvim-treesitter")
-    ts.setup({})
-
-    -- 3. Auto-enable highlighting and smart indentation for opened files
-    vim.api.nvim_create_autocmd("FileType", {
-        callback = function(args)
-            if ts.has_parser(args.match) then
-                vim.treesitter.start(args.buf)
-                vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-            end
-        end,
-    })
-end)
-
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function(args)
+    pcall(vim.treesitter.start)
+  end,
+})
